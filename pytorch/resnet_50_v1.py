b27cda2ef30d6ebc847d4bfd3edb2f9314e1d2e4
Qiyuan Gong <qiyuan.gong@intel.com>
2020-05-05 23:04:22 +0800

    Add metric

--- pytorch/resnet_50_v1.py
+++ pytorch/resnet_50_v1.py
@@ -1,5 +1,6 @@
 import torch
 from PIL import Image
+import time
 from torchvision import transforms
 
 # input_image = Image.open(filename)
@@ -32,21 +33,36 @@ def predict(model, image):
 def postprocessing(result):
     # Top-1
     preds = torch.topk(result, 1)
-    print(preds)
+    # print(preds)
     # Top-5
 
 def benchmark(model, image_path, batch_size=4, iterations=1):
     # create dummy data or read data from file path
     # input_batch = torch.rand(batch_size, 3, 224, 224) * 256
     input_image = Image.open(image_path)
+    start = time.time()
+    preprocessing_time = 0
+    predict_time = 0
+    postprocessing_time = 0
     for _ in range(iterations):
         # preprocessing
+        batch_start = time.time()
         model_input = preprocessing(input_image)
+        pre_end = time.time()
+        preprocessing_time += pre_end - batch_start
         # predict
         result = model.forward(model_input)
+        predict_end = time.time()
+        predict_time += predict_end - pre_end
         # postprocessing
         postprocessing(result)
-
+        postprocessing_time += time.time() - predict_end
+    total_time = time.time() - start
+    print("Total Running time %.2f s" % total_time)
+    print("Average batch time %.2f ms" % (total_time * 1000 / iterations))
+    print("Average preprocessing time %.2f ms" % (preprocessing_time * 1000 / iterations))
+    print("Average predict time %.2f ms" % (predict_time * 1000 / iterations))
+    print("Average postprocessing time %.2f ms" % (postprocessing_time * 1000 / iterations))
 
 if __name__ == '__main__':
     model = load_model()
