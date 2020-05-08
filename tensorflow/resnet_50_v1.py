e684a6edc6dc9af474089795c8bccdcb0df3fc72
Qiyuan Gong <qiyuan.gong@intel.com>
2020-05-08 16:04:30 +0800

    Change from keras to tensorflow.keras

--- tensorflow/resnet_50_v1.py
+++ tensorflow/resnet_50_v1.py
@@ -1,6 +1,7 @@
-from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+import tensorflow
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 import time
 
